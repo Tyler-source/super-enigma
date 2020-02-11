b16c8b571bc7644981858e7f8f2c1842fa565f08
Tyler-source <55996510+Tyler-source@users.noreply.github.com>
2020-02-10 22:23:41 -0500

    Fixed inToPost and PostToIn, eval broken.

--- BentleyT_CS304Assignment2.cpp
+++ BentleyT_CS304Assignment2.cpp
@@ -1,87 +1,164 @@
 #include <iostream>
 #include <string.h>
-#include<stack>
+#include <stack>
+#include <cmath>
 
 using namespace std;
 
 class Expression {
     public:
+        //contructor class
         Expression(string& input, int dir){
-            if (dir == 1)   
-                input = infix;
-            else if (dir == 2)
-                input = postfix;
+            if (dir == 1){ 
+                infix = input;
+                postfix = "";
+            }
+            else if (dir == 2){
+                postfix = input;
+                infix = "";
+            }
         }   
         //Denote Hiearchy of Operators.
-        int preced(string oper){
-            if(oper == "+" || oper == "-") return 1;
-            else if (oper == "*" || oper == "/") return 2;
-            else if (oper =="^") return 3;
+        int priority(char ch){
+            if(ch == '+' || ch == '-') return 1;
+            else if (ch == '*' || ch == '/') return 2;
+            else if (ch == '^') return 3;
             else return 0;
         }
-        string inToPost(string& input){
-            cout << "Converting in to Post \n";
-            stack<string> stk;                      //Create Stack
-            stk.push("#");                          //Place Holder
-            string postfix ="";                     //Create Output
+        string inToPost(){
+            stack<char> stk;        //create a char stack to place operators in the form of chars
+            stk.push('#');          //sentinel value             
+            string postfix = "";        
+            string::iterator it;    //iterator to go through every char of a the string 
 
-            //Loop to continue until input string is empty,
-            //everything will be on stack or in the output
-            while (!input.empty()){
-                cout << "the string is not empty \n";                
-                int pos = input.find(" ");
-                string str = input.substr(0, pos);
-
-                if (isdigit(str.front()))
-                    postfix += str;
-                else if(str == "(")
-                    stk.push(str);
-                else if(str == "^")
-                    stk.push(str);
-                else if(str == ")"){
-                    while(stk.top() != "#" && stk.top() != "(") {
-                    postfix += stk.top(); //store and pop until matching (
-                    stk.pop();
+            for(it = infix.begin(); it!=infix.end(); it++) {
+                if(isalnum(char(*it)))
+                    postfix += *it;      
+                else if(*it == '(')
+                    stk.push('(');
+                else if(*it == '^')
+                    stk.push('^');
+                else if(*it == ')') {       //put all values from stack into postfix until matching brace.
+                    while(stk.top() != '#' && stk.top() != '(') {
+                        postfix += stk.top(); 
+                        stk.pop();
+                    }
+                    stk.pop();          //remove the '(' from stack
+                }else {                 //push based on priority.
+                    if(priority(*it) > priority(stk.top()))
+                        stk.push(*it); 
+                    else {           //store and pop until higher priority is found
+                        while(stk.top() != '#' && priority(*it) <= priority(stk.top())) {
+                        postfix += stk.top();       
+                        stk.pop();
+                        }
+                        stk.push(*it);
                     }
                 }
-                else{
-                    if (preced(str) > preced(stk.top()))
-                        stk.push(str);
-                    else{
-                        while(stk.top() != "#") {
-                            postfix += stk.top();
-                            stk.pop();
-                        }
-                        stk.push(str);
-                    }
-                    stk.push(str);
-                } 
             }
-            while(stk.top() != "#"){
-                postfix += stk.top();
+                //pop and store the rest , until sentinel is found.
+            while(stk.top() != '#') {
+                postfix += stk.top();        
                 stk.pop();
             }
-            //ERROR: POSTFIX SEEMS TO BE empty
-            //FOLLOW WORKFLOW TO ID Issue.
             return postfix;
         }
-        string PostToIn();
-        double evaluate();
+        //Postfix to Infix
+        string PostToIn(){
+            stack<string> stk;      //create string stack , since we will be storing string sub expressions
+            for(int i = 0 ;postfix[i]!= '\0'; i++) {
+                    //if number , push to stack
+                if (isalnum(char(postfix[i]))){
+                    string str(1,postfix[i]);
+                    stk.push(str);
+                }
+                    //if not , pop 2 values off stack and push value1 "operand" value2.
+                else{
+                    string x = stk.top();
+                    stk.pop();
+                    string y = stk.top();
+                    stk.pop();
+                    stk.push("(" + x + postfix[i] + y + ")" );
+                } 
+            }
+            //whats left on the stack is our infix
+            return stk.top();
+        }
+        //Evaluate Function is not working, error when calling stod.
+        double evaluate(){
+            //Evaluate using postfix , so convert if currently infix 
+            if (postfix.empty()){
+                Expression expr(infix,1);
+                postfix = expr.inToPost();
+            } 
+            //stack for variables
+            stack<double> Dstk;
+            double i;
+            for(int i = 0 ;postfix.length()-1; i++) {
+                //if its a number , push to stack 
+                if (isalnum(char(postfix[i]))){
+                    string str(1,postfix[i]);
+                    double d = stod(str);
+                    Dstk.push(d);
+                }
+                else{
+                    //if an operator , pop 2 , perform the operation and push back to stack
+                    double x = Dstk.top();
+                    Dstk.pop();
+                    double y = Dstk.top();
+                    Dstk.pop();
+                    switch(char(postfix[i])){
+                        case '+': 
+                            i = x+y;
+                            Dstk.push(i);
+                            break;
+                        case '-': 
+                            i = x-y;
+                            Dstk.push(i);
+                            break;
+                        case '*': 
+                            i = x*y;
+                            Dstk.push(i);
+                            break;
+                        case '/': 
+                            if (y == 0)
+                                return -99;
+                            i = x/y;
+                            Dstk.push(i);
+                            break;
+                        case '^': 
+                            i = pow(x,y);
+                            Dstk.push(i);
+                            break;
+                    }
+                }
+            }
+            //remaining value will be result of the expression
+            return Dstk.top();
+        }
 
-
+        
 
     private:
         string infix;
         string postfix;
 };
 
-
+//driver code
 int main(){
-    cout << "Please Enter Expression seperated by spaces (Ex: \"x y + ( z ^ w ) \")";
+    cout << "Are you entering an infix or postfix expression? (1 = infix / 2 = postfix) : ";
+    int dir;
+    cin >> dir;
+    cout << "Please Enter Expression (Ex: \" xyzw +-*/^ () \") :";
     string input;
     cin >> input;
-    Expression string = Expression(input , 1);
-    string.inToPost(input);
-    cout << input;
+    Expression expr = Expression(input , dir);
+    string result;
+    if(dir == 1)
+       result = expr.inToPost();
+    else
+        result = expr.PostToIn();
+    cout << result << "\n";
+    //cout << "Expression result is : " << expr.evaluate();
     return 0;
 }
